c22663d0d0e0f5f8910f3c293ded0fbe386ddad1
Kuldeep <kuldeepsingh41204@gmail.com>
2026-01-18 13:58:20 +0530

    Initial Commit

---
+++ WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by kuldeep Singh on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
